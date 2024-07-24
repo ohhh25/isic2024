e9cfd1055f8443252b7f0fa79e7c9fee83fd71d0
Lukas Cao <lukascao06@gmail.com>
2024-07-24 15:51:17 -0700

    Update training.py
    Without Loop

--- training.py
+++ training.py
@@ -1,16 +1,15 @@
 import numpy as np
 import pandas as pd
+from PIL import Image
 
 import os
+import h5py
+import io
 
 import torch
 from torchvision.models import resnet18, ResNet18_Weights
-from torchvision.transforms import v2
-
-device = torch.device("mps")
-model = resnet18(weights=ResNet18_Weights.DEFAULT).to(device)
-
-transforms = v2.Compose([ResNet18_Weights.IMAGENET1K_V1.transforms])
+import torch.nn as nn
+import torch.optim as optim
 
 def get_statistics(name, set, cases=["Benign", "Malignant"]):
     size = len(set)
@@ -45,6 +44,15 @@ def reload(path):
     val_n = get_statistics("Validation Set", val_gt)
     return (train_gt, val_gt), (train_n, val_n)
 
+def load_batch(hdf5, gt_set, i, size=256):
+    gt = gt_set[i:i+size]
+    keys, y = list(gt[:, 0]), np.array(gt[:, 1], dtype=int)
+    with h5py.File(hdf5, "r") as f:
+        imgs = [transforms(Image.open(io.BytesIO(f.get(key)[()]))) for key in keys]
+    X = torch.stack(imgs).to(device)
+    y = torch.tensor(y, dtype=torch.float32).unsqueeze(1).to(device)
+    return X, y
+
 if not os.path.isfile("Data/split.npz"):
     print("Creating New Train/Val Split...saves to Data/split.npz")
     (train_gt, val_gt), (train_n, val_n) = preload("Data/train-metadata.csv", "Data/split.npz")
@@ -52,4 +60,18 @@ else:
     print("Loading Previously Created Train/Val Split...from Data/split.npz")
     (train_gt, val_gt), (train_n, val_n) = reload("Data/split.npz")
 
-print(model)
+model = resnet18(weights=ResNet18_Weights.DEFAULT)
+model.fc = nn.Linear(model.fc.in_features, 1)
+
+device = torch.device("mps")
+model = model.to(device)
+loss_fn = torch.nn.BCEWithLogitsLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+transforms = ResNet18_Weights.IMAGENET1K_V1.transforms()
+
+X, y = load_batch("Data/train-image.hdf5", train_gt, 0)
+outputs = model(X)
+loss = loss_fn(outputs, y)
+print(loss.item())
+loss.backward()
+optimizer.step()
